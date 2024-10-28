8c4867f7263200dadebe69655f593d6e7eb9ed5d
Hamza Albanna <hamzaalbanna02@gmail.com>
2024-10-28 10:07:40 +0300

    two_sum

---
+++ PalindromeNamber.py
@@ -0,0 +1,11 @@
+# class Solution(object):
+#     def isPalindrome(self, x):
+#         for i in len(x):
+#             if x[i]!= x[-i]:
+#                 return False
+            
+# d=125521
+# r=d[::-1]
+# if d[1]==r[1]:
+#     print(True)
+
--- two_sum.py
+++ two_sum.py
@@ -7,4 +7,3 @@ class Solution(object):
                 if ss == target:
                     return [i, g]# return the index not the numbers --> [nums[i], nums[g]]
 ret = Solution().twoSum([2,7,11,15], 9)
-print(ret)
